253ccaac2cb8281c28436e366bb8759469ed6cdc
nampx-0806 <xnam7979@gmail.com>
2019-04-11 13:42:11 +0700

    participan detail and search

--- resources/js/components/vote/tab/tabResult/tabResultChildren/ResultByTable/ResultByTable.js
+++ resources/js/components/vote/tab/tabResult/tabResultChildren/ResultByTable/ResultByTable.js
@@ -2,31 +2,98 @@ import React, { Component } from "react";
 import autoBind from "react-autobind"
 import RowResult from './RowResult'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import RowResultModal from './RowResultModal'
 
 class ResultByTable extends Component {
     constructor(props) {
         super(props);
         autoBind(this)
+        this.state = {
+            searchString: '',
+        }
+    }
+    handleChangeInput(e) {
+        this.setState({
+            searchString: e.target.value
+        })
     }
     render() {
         let maxCountOption = 0;
         let serialOption = 0;
-        const participantVote = this.props.participantVote;
-        this.props.pollOption.map(function (pollOption) {
+        const pollId = this.props.pollId;
+        const pollOption = this.props.pollOption;
+        var participantVote = this.props.participantVote,
+            searchString = this.state.searchString.trim().toLowerCase();
+        if (searchString.length > 0) {
+            participantVote = participantVote.filter(function (i) {
+                return i.participant.name.toLowerCase().match(searchString);
+            });
+        }
+        pollOption.map(function (pollOption) {
             const countByOption = participantVote.filter((countVote) => countVote.option_id === pollOption.id).length;
             if (countByOption > maxCountOption) {
                 maxCountOption = countByOption;
             }
         })
-        const rowResult = this.props.pollOption.map((pollOption, i) => {
+        const rowResult = pollOption.map((pollOption, i) => {
             serialOption++;
             const countByOption = participantVote.filter((countVote) => countVote.option_id === pollOption.id).length;
             return <RowResult key={i} serialOption={serialOption} pollOption={pollOption} countByOption={countByOption} maxCountOption={maxCountOption} numerical={i} />
         })
 
+        const nameOption = pollOption.map(function (option) {
+            return <React.Fragment key={option.id}>
+                <th className="table-optionName">{option.name}</th>
+            </React.Fragment>
+        });
+        const rowResultModal = participantVote.map(function (participantVote) {
+            if (participantVote.option.poll_id === pollId) {
+                return <RowResultModal key={participantVote.id} participantVote={participantVote} pollOption={pollOption} />
+            }
+        });
+        const countByOption = pollOption.map(function (option) {
+            const countByOption = participantVote.filter((countVote) => countVote.option_id === option.id).length;
+            return <React.Fragment key={option.id}><th className="table-result">{countByOption}</th></React.Fragment>
+        });
+        const countParticipantVote = participantVote.filter((countVote) => countVote.option.poll_id === this.props.pollId).length;
         return (
             <React.Fragment>
                 <div className={this.props.tabResult === "tab-table" ? "tab-result-table" : "tab-result-table tab-none"}>
+                    <a className="btn10" data-toggle="modal" data-target="#resultModal">
+                        <i className="fa fa-eye"></i>
+                        <span>Xem chi tiết bầu chọn</span>
+                        <div className="transition"></div>
+                    </a>
+                    <div className="modal fade" id="resultModal" role="dialog">
+                        <div className="modal-dialog modal-lg">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <input type="search" placeholder="Search" value={this.state.searchString} onChange={this.handleChangeInput} />
+                                </div>
+                                <div className="modal-body">
+                                    <table className="table table-striped table-bordered table-hover">
+                                        <thead>
+                                            <tr className="table-head">
+                                                {countByOption}
+                                                <th colSpan="3" style={{ "textAlign": "center" }}> {countParticipantVote} Người bầu chọn</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr className="table-head">
+                                                {nameOption}
+                                                <td className="table-name bold" colSpan="2">Tên</td>
+                                                <td className="table-name bold">Email</td>
+                                            </tr>
+                                            {rowResultModal}
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div className="table-result">
                         <div className="table-result-head">
                             <div className="cell-table">
--- resources/js/components/vote/tab/tabResult/tabResultChildren/ResultByTable/RowResultModal.js
+++ resources/js/components/vote/tab/tabResult/tabResultChildren/ResultByTable/RowResultModal.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import autoBind from "react-autobind";
 
-class RowVote extends Component {
+class RowResultModal extends Component {
     constructor(props) {
         super(props);
         autoBind(this);
@@ -11,7 +11,7 @@ class RowVote extends Component {
         const optionId = this.props.participantVote.option_id;
         const participantName = this.props.participantVote.participant.name;
         const participantEmail = this.props.participantVote.participant.email;
-        const rowVote = this.props.pollOption.map(function (option) {
+        const rowResultModal = this.props.pollOption.map(function (option) {
             return <React.Fragment key={option.id}>
                 <td>{option.id === optionId ? <i className="fa fa-check"></i> : ''}</td>
             </React.Fragment>
@@ -19,18 +19,12 @@ class RowVote extends Component {
         return (
             <React.Fragment>
                 <tr>
-                    <th className="table-option">
-                        <div className="">
-                            <a href="#" className="fa fa-pencil"></a>
-                            <a href="#" className="fa fa-trash"></a>
-                        </div>
-                    </th>
-                    <td className="table-name">{participantName}</td>
+                    {rowResultModal}
+                    <td className="table-name" colSpan="2">{participantName}</td>
                     <td className="table-name">{participantEmail}</td>
-                    {rowVote}
                 </tr>
             </React.Fragment >
         );
     }
 }
-export default RowVote;
+export default RowResultModal;
